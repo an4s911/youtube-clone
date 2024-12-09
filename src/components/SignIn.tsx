ca9ebfeb389bf840daa4020223d065e4b61a0aed
Anas Bashir <nasbas23@gmail.com>
2024-12-10 01:53:34 +0300

    Show loading while signing in

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { SiYoutube } from "@icons-pack/react-simple-icons";
 import { FormEvent, useState } from "react";
 import { Navigate, useNavigate } from "react-router";
+import { Loader2 } from "lucide-react";
 
 type SignInProps = {
     isLoggedIn: boolean;
@@ -12,10 +13,12 @@ type SignInProps = {
 function SignIn({ isLoggedIn, setUserName, setIsLoggedIn }: SignInProps) {
     const [emailValue, setEmailValue] = useState("test@test.com");
     const [passwordValue, setPasswordValue] = useState("password123");
+    const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
 
     const handleSignIn = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setIsLoading(true);
         const URL = import.meta.env.VITE_API_URL;
         const formData = new FormData(e.currentTarget);
         const data = {
@@ -41,7 +44,8 @@ function SignIn({ isLoggedIn, setUserName, setIsLoggedIn }: SignInProps) {
             })
             .catch((err) => {
                 console.log(err.response.data.error);
-            });
+            })
+            .finally(() => setIsLoading(false));
     };
 
     // Check if user is already logged in
@@ -51,7 +55,7 @@ function SignIn({ isLoggedIn, setUserName, setIsLoggedIn }: SignInProps) {
 
     return (
         <div className="flex justify-center items-center h-full">
-            <div className="px-20 py-10 mx-auto border rounded-md border-white flex flex-col justify-center items-center bg-gray-800 w-max sm:w-96 h-2/3 gap-10">
+            <div className="relative px-20 py-10 mx-auto border rounded-md border-white flex flex-col justify-center items-center bg-gray-800 w-max sm:w-96 h-2/3 gap-10">
                 <div className="flex flex-col items-center justify-center gap-5">
                     <div className="logo">
                         <SiYoutube size={50} color="#FF0000" title="Youtube" />
@@ -60,6 +64,11 @@ function SignIn({ isLoggedIn, setUserName, setIsLoggedIn }: SignInProps) {
                         <h1 className="text-3xl font-semibold">Sign in</h1>
                     </div>
                 </div>
+                {isLoading && (
+                    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-max h-max">
+                        <Loader2 className="animate-spin h-20 w-20 opacity-50" />
+                    </div>
+                )}
                 <form
                     className="signin-form flex flex-col items-center gap-10 w-full"
                     onSubmit={handleSignIn}
@@ -72,6 +81,7 @@ function SignIn({ isLoggedIn, setUserName, setIsLoggedIn }: SignInProps) {
                             placeholder="Email"
                             value={emailValue}
                             onChange={(e) => setEmailValue(e.target.value)}
+                            disabled={isLoading}
                             required
                         />
                         <input
@@ -81,12 +91,14 @@ function SignIn({ isLoggedIn, setUserName, setIsLoggedIn }: SignInProps) {
                             placeholder="Password"
                             value={passwordValue}
                             onChange={(e) => setPasswordValue(e.target.value)}
+                            disabled={isLoading}
                             required
                         />
                     </div>
                     <button
                         type="submit"
                         className="rounded-md bg-blue-600 px-10 py-3"
+                        disabled={isLoading}
                     >
                         SignIn
                     </button>
