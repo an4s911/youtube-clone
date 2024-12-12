e13d51c56b6e0b2667dfe5746696c9d5a5037064
Anas Bashir <nasbas23@gmail.com>
2024-12-12 09:31:44 +0300

    Add anon view for users not logged in

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Link, Route, Routes } from "react-router";
 import SignIn from "./SignIn";
 
 type ContentProps = {
@@ -7,6 +7,19 @@ type ContentProps = {
     setUserName: (value: string) => void;
     setEmail: (value: string) => void;
 };
+
+function AnonView() {
+    return (
+        <div className="w-full flex justify-center py-10">
+            <div className="px-10 md:px-24 py-6 text-center bg-gray-800 rounded-lg border border-gray-700">
+                <h4 className="text-xl font-semibold">You are not signed in</h4>
+                <p>
+                    Please <Link to="/signin" className="text-blue-500">sign in</Link> to continue
+                </p>
+            </div>
+        </div>
+    );
+}
 
 function Content({
     isLoggedIn,
@@ -17,7 +30,10 @@ function Content({
     return (
         <main>
             <Routes>
-                <Route path="/" element={<h1>Content</h1>} />
+                <Route
+                    path="/"
+                    element={isLoggedIn ? <h1>Content</h1> : <AnonView />}
+                />
                 <Route
                     path="/signin"
                     element={
